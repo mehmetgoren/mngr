b43d80a09354db15ed2b4e8cccbb5b0f2f4c3a0e
Gokalp Goren <mehmetgoren@outlook.com>
2022-02-02 20:47:42 +0000

    Adding FFmpeg builder part IV

--- api/streaming.go
+++ api/streaming.go
@@ -7,6 +7,17 @@ import (
 )
 
 func RegisterStreamingEndpoints(router *gin.Engine) {
+
+	router.GET("/streaming", func(c *gin.Context) {
+		models, err := utils.StreamingRep.GetAll()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+
+		c.JSON(http.StatusOK, models)
+	})
+
 	router.GET("/streaming/:id", func(ctx *gin.Context) {
 		id := ctx.Param("id")
 		source, err := utils.StreamingRep.Get(id)
--- reps/streaming_repository.go
+++ reps/streaming_repository.go
@@ -25,3 +25,29 @@ func (r *StreamingRepository) Get(id string) (*models.StreamingModel, error) {
 
 	return &model, nil
 }
+
+func (r *StreamingRepository) GetAll() ([]*models.StreamingModel, error) {
+	conn := r.Connection
+	keys, err := conn.Keys(context.Background(), redisKeyStreaming+"*").Result()
+	if err != nil {
+		if err.Error() == "redis: nil" {
+			emptyList := make([]*models.StreamingModel, 0)
+			conn.Set(context.Background(), redisKeyStreaming, emptyList, 0)
+			return emptyList, nil
+		} else {
+			log.Println("Error getting all streaming from redis: ", err)
+			return nil, err
+		}
+	}
+	list := make([]*models.StreamingModel, 0, 5)
+	for _, key := range keys {
+		var p models.StreamingModel
+		err := conn.HGetAll(context.Background(), key).Scan(&p)
+		if err != nil {
+			log.Println("Error getting streaming from redis: ", err)
+			return nil, err
+		}
+		list = append(list, &p)
+	}
+	return list, nil
+}
