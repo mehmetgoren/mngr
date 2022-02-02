649ec8dbcb4d3f7c7845de3300df05462b826ceb
Gokalp Goren <mehmetgoren@outlook.com>
2022-02-02 02:41:04 +0000

    Adding FFmpeg builder part III

--- models/streaming_model.go
+++ models/streaming_model.go
@@ -13,15 +13,17 @@ type StreamingModel struct {
 	Args        string `json:"args" redis:"args"`
 	FailedCount int    `json:"failedCount" redis:"failedCount"`
 
-	StreamingType         int    `json:"streaming_type" redis:"streaming_type"`
-	RtmpServerInitialized bool   `json:"rtmp_server_initialized" redis:"rtmp_server_initialized"`
-	RtmpServerType        int    `json:"rtmp_server_type" redis:"rtmp_server_type"`
-	RtmpImageName         string `json:"rtmp_image_name" redis:"rtmp_image_name"`
-	RtmpContainerName     string `json:"rtmp_container_name" redis:"rtmp_container_name"`
-	RtmpAddress           string `json:"rtmp_address" redis:"rtmp_address"`
-	RtmpFlvAddress        string `json:"rtmp_flv_address" redis:"rtmp_flv_address"`
-	RtmpContainerPorts    string `json:"rtmp_container_ports" redis:"rtmp_container_ports"`
-	RtmpContainerCommands string `json:"rtmp_container_commands" redis:"rtmp_container_commands"`
+	StreamingType           int    `json:"streaming_type" redis:"streaming_type"`
+	RtmpServerInitialized   bool   `json:"rtmp_server_initialized" redis:"rtmp_server_initialized"`
+	RtmpServerType          int    `json:"rtmp_server_type" redis:"rtmp_server_type"`
+	FlvPlayerConnectionType int    `json:"flv_player_connection_type" redis:"flv_player_connection_type"`
+	NeedReloadInterval      int    `json:"need_reload_interval" redis:"need_reload_interval"`
+	RtmpImageName           string `json:"rtmp_image_name" redis:"rtmp_image_name"`
+	RtmpContainerName       string `json:"rtmp_container_name" redis:"rtmp_container_name"`
+	RtmpAddress             string `json:"rtmp_address" redis:"rtmp_address"`
+	RtmpFlvAddress          string `json:"rtmp_flv_address" redis:"rtmp_flv_address"`
+	RtmpContainerPorts      string `json:"rtmp_container_ports" redis:"rtmp_container_ports"`
+	RtmpContainerCommands   string `json:"rtmp_container_commands" redis:"rtmp_container_commands"`
 
 	Recording      bool `json:"recording" redis:"recording"`
 	RecordDuration int  `json:"record_duration" redis:"record_duration"`
